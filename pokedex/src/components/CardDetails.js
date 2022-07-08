6cfa3ed99adbe3feb982eb151199f22d2920bab8
Fernanda da Silva <nanda.drew01@gmail.com>
2022-07-08 12:01:54 -0300

    estilizando pagina detalhes

--- pokedex/src/components/CardDetails.js
+++ pokedex/src/components/CardDetails.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { useRequestDetails } from '../hooks/useRequestDetails';
 import { BASE_URL } from '../constants/urls';
-import {
-  Container,
-  ContainerDetails,
-  Column1,
-  ContainerColumn1,
-} from './styles';
+import { Parent, Div1, Div2, Div3, Div4, Div5, Img } from './styles';
 
 export const CardDetails = () => {
   const [data, error, isLoading] = useRequestDetails(
@@ -14,32 +9,40 @@ export const CardDetails = () => {
   );
 
   return (
-    <Container>
+    <Parent>
       {isLoading && <p>Carregando</p>}
       {!isLoading && error && <p>Ocorreu um erro</p>}
       {!isLoading && data && (
-        <ContainerDetails>
-          <ContainerColumn1>
-            <Column1>
-              <p>{data.species.name}</p>
-              <p>Imagem frontal </p>
-            </Column1>
-            <Column1>
-              <p>{data.species.name}</p>
-              <p>Imagem de costas </p>
-            </Column1>
-          </ContainerColumn1>
-          <div>
-            <p> Stats </p>
-          </div>
-          <div>
-            <p>type 1 </p>
-          </div>
-          <div>
-            <p>moves</p>
-          </div>
-        </ContainerDetails>
+        <>
+          <Div1>
+            <Img src={data.sprites.front_default} alt={data.species.name} />
+          </Div1>
+          <Div2>
+            <Img src={data.sprites.back_default} alt={data.species.name} />
+          </Div2>
+          <Div3>
+            <h2> Poderes </h2>
+            <p> Hp: {data.stats[0].base_stat} </p>
+            <p> attack: {data.stats[1].base_stat} </p>
+            <p> defense:{data.stats[2].base_stat} </p>
+            <p> special-attack:{data.stats[3].base_stat} </p>
+            <p> special-defense:{data.stats[4].base_stat} </p>
+            <p> speed:{data.stats[5].base_stat} </p>
+          </Div3>
+          <Div4>
+            <p>{data.types[0].type.name}</p>
+            <p>{data.types[1].type.name}</p>
+          </Div4>
+          <Div5>
+            <h2>Principais ataques</h2>
+            <p>{data.moves[0].move.name}</p>
+            <p>{data.moves[1].move.name}</p>
+            <p>{data.moves[2].move.name}</p>
+            <p>{data.moves[3].move.name}</p>
+            <p>{data.moves[4].move.name}</p>
+          </Div5>
+        </>
       )}
-    </Container>
+    </Parent>
   );
 };
--- pokedex/src/components/styles.js
+++ pokedex/src/components/styles.js
@@ -15,21 +15,58 @@ export const Conteudo = styled.div`
 `;
 
 export const ContainerDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-export const Column1 = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px solid black;
-  margin: 0px 40px;
-  padding: 20px;
-  width: 90%;
-  margin-top: 20%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+  justify-items: center div {
+    border: 1px solid black;
+  }
 `;
 
-export const ContainerColumn1 = styled.div`
+export const Parent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 14px;
+  margin: 38px;
+  height: 73vh;
+  @media (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+export const Img = styled.img`
+  width: 50%;
+`;
+
+export const Div1 = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+  padding: 10px;
   display: flex;
-  flex-direction: column;
-  margin-top: 10%;
+  justify-content: center;
+`;
+export const Div2 = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+`;
+export const Div3 = styled.div`
+  grid-area: 1 / 2 / 3 / 3;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
+  padding: 10px;
+`;
+export const Div4 = styled.div`
+  grid-area: 1 / 3 / 2 / 4;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+
+  padding: 10px;
+`;
+export const Div5 = styled.div`
+  grid-area: 2 / 3 / 3 / 4;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+
+  padding: 10px;
 `;
