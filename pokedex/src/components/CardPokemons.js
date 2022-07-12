d4cd26d9e4f11233902eeab03bca532f1f3169e7
Fernanda da Silva <nanda.drew01@gmail.com>
2022-07-12 09:28:23 -0300

    finalizando pagina de detalhes

--- pokedex/src/components/CardPokemons.js
+++ pokedex/src/components/CardPokemons.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import { useRequestData } from '../hooks/useRequestData';
 import { BASE_URL } from '../constants/urls';
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { goToDetails } from '../routes/coordinator';
-import { Container, Conteudo } from './styles';
-
-const Buttons = styled.div`
-  display: flex;
-  text-align: center;
-  align-items: center;
-  z-index: 2;
-  flex-direction: column;
-  background-color: #0077b6;
-  padding: 5px;
-  border-radius: 10px;
-  gap: 5px;
-  button {
-    padding: 4px 8px;
-    border-radius: 4px;
-    border: none;
-    border: 1px solid white;
-    color: #00509d;
-    background-color: white;
-    font-weight: bold;
-    cursor: pointer;
-  }
-  h3 {
-    font-weight: bold;
-    color: white;
-  }
-`;
-
+import { Container, Conteudo, Buttons } from './styles';
 export const CardPokemons = () => {
   const navigate = useNavigate();
   const [pokemons, error, isLoading] = useRequestData(`${BASE_URL}pokemon/`);
